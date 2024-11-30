d9fbac6ecb5f519fa3beb86afcf140e63296d940
Kipyego_t <141511043+timon-yego@users.noreply.github.com>
2024-11-30 23:01:38 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,5 @@
 -- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+-- Ordered by booking start date and user last name
 SELECT 
     b.booking_id, 
     u.user_id, 
@@ -13,9 +14,12 @@ FROM
 INNER JOIN 
     User u
 ON 
-    b.user_id = u.user_id;
+    b.user_id = u.user_id
+ORDER BY 
+    b.start_date ASC, u.last_name ASC;
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties with no reviews
+-- Ordered by property name and review rating
 SELECT 
     p.property_id, 
     p.name AS property_name, 
@@ -27,9 +31,12 @@ FROM
 LEFT JOIN 
     Review r
 ON 
-    p.property_id = r.property_id;
+    p.property_id = r.property_id
+ORDER BY 
+    p.name ASC, r.rating DESC;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+-- Ordered by user last name and booking start date
 -- Note: FULL OUTER JOIN is not supported in some databases like MySQL. Use UNION of LEFT JOIN and RIGHT JOIN as a workaround.
 SELECT 
     u.user_id, 
@@ -57,4 +64,6 @@ FROM
 RIGHT JOIN 
     Booking b
 ON 
-    u.user_id = b.user_id;
+    u.user_id = b.user_id
+ORDER BY 
+    u.last_name ASC, b.start_date ASC;
